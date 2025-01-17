b49b38af31764f8960a6d7f31bc1c38e6c1d3222
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-01-17 10:50:56 +0800

    removed console log

--- src/credentials.js
+++ src/credentials.js
@@ -1,7 +1,7 @@
 const credentials = {
 	clientId: "",
 	scopes: "chat:read chat:edit channel:read:redemptions user:read:email",
-	channel: [""], // your main channel
+	channel: "", // your main channel
 	sender: "", // bot username
 };
 
